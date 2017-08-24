9c7ba808912790592fff383a5fb4c4e14db57fbe
Esteban Luchsinger <eluchsingerm@hotmail.com>
2017-08-24 17:57:56 +0200

    Better examples, less space wasted

--- sourcecode/csharp/example_controller.cs
+++ sourcecode/csharp/example_controller.cs
@@ -1,6 +1,22 @@
-public class HomeController : Controller {
-    // GET /home/index
-    public ActionResult Index() {
-        return View();
+[Route("accounts")]
+[Authorize]
+public class AccountController {
+    AccountService accSvc;
+    public AccountController(AccountService accountService)
+    { this.accSvc = accountService; }
+
+    [HttpGet("{accountNr}")]
+    public AccountViewModel Get(string accNr)
+    {
+        return new AccountViewModel(accSvc.GetAccount(accNr));
+    }
+
+    [HttpGet("transactions")]
+    public TransactionSearchResult GetTransactions(
+        [FromQuery]TransactionSearchQuery query)
+    {
+        return accSvc.GetTransactions(
+            User.FindFirst(SecurityClaims.AccountIdClaim).Value,
+             query);
     }
 }
